4fd9b5ce04dd2885b7eca27e0d92312a9f97ca76
KGPP <kianpadua124@gmail.com>
2026-02-14 10:12:05 +0800

    DSGDSFSD

--- valentines-site/src/App.tsx
+++ valentines-site/src/App.tsx
@@ -34,35 +34,35 @@ function App() {
         </div>
       )}
 
-      {page === 4 && (
-        <div className="page">
-          <h1>i just want to tell you...</h1>
-          <button onClick={() => setPage(5)}>Continue</button>
-        </div>
-      )}
-
-      {page === 4 && (
-        <div className="page">
-          <h1>Wala akong balak mag bago BWHAHAHAHAHA</h1>
-          <button onClick={() => setPage(5)}>Continue</button>
-        </div>
-      )}
-
       {page === 5 && (
         <div className="page">
-          <h1>Thank you for being my friend for so long</h1>
-          <button onClick={() => setPage(6)}>Continue</button>
-        </div>
-      )}
-
-      {page === 5 && (
-        <div className="page">
-          <h1>I do really appreciate you a lot</h1>
+          <h1>I just want to tell you...</h1>
           <button onClick={() => setPage(6)}>Continue</button>
         </div>
       )}
 
       {page === 6 && (
+        <div className="page">
+          <h1>WALA AKONG BALAK MAG BAGO BWHAHAHAHAHA</h1>
+          <button onClick={() => setPage(7)}>Continue</button>
+        </div>
+      )}
+
+      {page === 7 && (
+        <div className="page">
+          <h1>Thank you for being my friend for so long</h1>
+          <button onClick={() => setPage(8)}>Continue</button>
+        </div>
+      )}
+
+      {page === 8 && (
+        <div className="page">
+          <h1>I do really appreciate you a lot</h1>
+          <button onClick={() => setPage(9)}>Continue</button>
+        </div>
+      )}
+
+      {page === 9 && (
         <div className="page">
           <h1>Happy Valentine’s Day Shaina!❤️</h1>
         </div>
